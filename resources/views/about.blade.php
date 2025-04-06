9f98674fbd6aeab64fbf4d6ae5d86ef57daef81f
Kuldeep Verma <kv5228920@gmail.com>
2025-04-06 17:46:44 +0530

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>About Page</h1>
+    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h1>Home Page</h1>
+
+    <h3>{{ URL::current() }}</h3>
+
+    <h3>{{ URL::full() }}</h3>
+    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+</div>
